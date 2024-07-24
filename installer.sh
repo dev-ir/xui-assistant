35defdbf0314f73f08042a9cfbb0ed93520a8c37
unknown <info@developermen.ir>
2024-07-24 04:40:21 +0330

    Installer file

---
+++ installer.sh
@@ -0,0 +1,14 @@
+# install git 
+sudo apt install git -y
+
+# clone from repo in xui-assistant directory
+git clone https://github.com/dev-ir/xui-assistant.git /root/xui-assistant/
+
+# Access Folder
+sudo chmod +x /root/xui-assistant/
+
+# Change Directory
+cd /root/xui-assistant/
+
+# Run Script
+sudo bash main.sh
