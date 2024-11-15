2e04af032006fe13d5413a292a8d8a9b54823142
unknown <info@developermen.ir>
2024-11-16 02:52:07 +0330

    Access to menu after install

--- installer.sh
+++ installer.sh
@@ -1,7 +1,11 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+NC='\033[0m' # No Color
 #Remove Old version
 rm -rf /root/xui-assistant/
 
-# install git 
+# install git
 sudo apt install git -y
 
 # clone from repo in xui-assistant directory
@@ -14,6 +18,11 @@ chmod +x /root/xui-assistant/menu.sh
 
 sudo mv /root/xui-assistant/menu.sh /usr/local/bin/xui-assis
 
-echo "You can now access the menu permanently by using the command `xui-assis`."
+clear
+echo "+-------------------------------------------------------------------------------------+"
+echo -e "|  Telegram Channel : ${RED}@DVHOST_CLOUD ${NC} |  YouTube : ${RED}youtube.com/@dvhost_cloud${NC} "
+echo "+-------------------------------------------------------------------------------------+"
+echo -e "| You can now permanently access the menu by typing: ${GREEN}xui-assis${NC}    |  Version : ${GREEN} 2.0${NC} "
+echo "+-------------------------------------------------------------------------------------+"
 
 # xui-assis
