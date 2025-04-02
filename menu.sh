6fef4c249e4311d5df02b8f64308b32206b4672e
dev-ir <info@developermen.ir>
2025-04-02 22:29:17 +0330

    refactor: optimize menu script

--- menu.sh
+++ menu.sh
@@ -1,119 +1,141 @@
 #!/bin/bash
 
+# Color Definitions
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
+VERSION='2.3.1'
 
+# Path Definitions
+XUI_ASSISTANT_DIR="/root/xui-assistant"
+XUI_DB_PATH="/etc/x-ui/x-ui.db"
 
-# [[ $EUID -ne 0 ]] && echo -e "${RED}Fatal error: ${RED} Please run this script with root privilege ${NC}  \n " && exit 1
+# Check root privilege
+[[ $EUID -ne 0 ]] && echo -e "${RED}Fatal error: Please run this script with root privilege${NC}\n" && exit 1
 
-XUI_ASSISTANT_MENU(){
+xui_assis_display_menu() {
     clear
-
-    XUI_CORE=$(check_xui_exist)
+    local xui_status=$(xui_assis_check_installation)
     
-    echo '+-----------------------------------------------------------------------------------------+'
-	echo '| Y88b   d88P 888     888 8888888            d8888  .d8888b.   .d8888b. 8888888 .d8888b.  |'
-	echo '|  Y88b d88P  888     888   888             d88888 d88P  Y88b d88P  Y88b  888  d88P  Y88b |'
-	echo '|   Y88o88P   888     888   888            d88P888 Y88b.      Y88b.       888  Y88b.      |'
-	echo '|    Y888P    888     888   888           d88P 888  "Y888b.    "Y888b.    888   "Y888b.   |'
-	echo '|    d888b    888     888   888          d88P  888     "Y88b.     "Y88b.  888      "Y88b. |'
-	echo '|   d88888b   888     888   888  888888 d88P   888       "888       "888  888        "888 |'
-	echo '|  d88P Y88b  Y88b. .d88P   888        d8888888888 Y88b  d88P Y88b  d88P  888  Y88b  d88P |'
-	echo '| d88P   Y88b  "Y88888P"  8888888     d88P     888  "Y8888P"   "Y8888P" 8888888 "Y8888P"  |'
-    echo '+-----------------------------------------------------------------------------------------+'
-    echo -e "| Telegram Channel : ${YELLOW}@DVHOST_CLOUD ${NC} | YouTube : ${RED}@dvhost_cloud${NC}|    Version : ${GREEN} 2.3${NC} "
-    echo '+-----------------------------------------------------------------------------------------+'
-    echo -e "|${YELLOW}Please choose an option:${NC}"
-    echo '+-----------------------------------------------------------------------------------------+'
-    echo -e $1
-    echo '+-----------------------------------------------------------------------------------------+'
-    echo -e "\033[0m"
+    cat << "EOF"
++-----------------------------------------------------------------------------------------+
+| Y88b   d88P 888     888 8888888            d8888  .d8888b.   .d8888b. 8888888 .d8888b.  |
+|  Y88b d88P  888     888   888             d88888 d88P  Y88b d88P  Y88b  888  d88P  Y88b |
+|   Y88o88P   888     888   888            d88P888 Y88b.      Y88b.       888  Y88b.      |
+|    Y888P    888     888   888           d88P 888  "Y888b.    "Y888b.    888   "Y888b.   |
+|    d888b    888     888   888          d88P  888     "Y88b.     "Y88b.  888      "Y88b. |
+|   d88888b   888     888   888  888888 d88P   888       "888       "888  888        "888 |
+|  d88P Y88b  Y88b. .d88P   888        d8888888888 Y88b  d88P Y88b  d88P  888  Y88b  d88P |
+| d88P   Y88b  "Y88888P"  8888888     d88P     888  "Y8888P"   "Y8888P" 8888888 "Y8888P"  |
++-----------------------------------------------------------------------------------------+
+EOF
+echo -e "| Telegram Channel : ${YELLOW}@DVHOST_CLOUD${NC} | YouTube : ${RED}@dvhost_cloud${NC} | Version : ${GREEN}${VERSION}${NC} "
+echo '+-----------------------------------------------------------------------------------------+'
+echo -e "|${YELLOW} Please choose an option:${NC}"
+echo '+-----------------------------------------------------------------------------------------+'
+echo -e "$1"
+echo '+-----------------------------------------------------------------------------------------+'
 }
 
-# Main menu function
-loader() {
-    XUI_ASSISTANT_MENU "|1  - Copy Database to Destination VPS\n|2  - Send Gift to All Clients\n|3  - User Management\n|4 - Set Cronjob for Resetting Xray\n|5 - Fix WhatsApp Time\n|6 - Install WordPress\n|7 - Block All Speedtest Requests\n|8 - XUI Bot (${RED}Multi-Server Support${NC})\n|9  - XUI Managment\n|10 - Uninstall\n|11 - Exit"
-
-    read -p "| Enter option number: " choice
-    case $choice in
-        1) transfer_db ;;
-        2) gift_user ;;
-        3) manage_users ;;
-        4) xray_restart ;;
-        5) fix_timezone ;;
-        6) install_wordpress ;;
-        7) block_speedtest_sites ;;
-        8) xui_bot ;;
-        9) xui_managment ;;
-        10) uninstall ;;
-        0) exit_program ;;
-        *) echo -e "${RED}Invalid option. Try again.${NC}" ;;
-    esac
+xui_assis_check_installation() {
+    [[ -f "$XUI_DB_PATH" ]] && echo "${GREEN}Installed${NC}" || echo "${RED}Not installed${NC}"
 }
 
-# Functions
-gift_user() {
-    python3 /root/xui-assistant/core/day_size.py
+# Core Functions
+xui_assis_transfer_database() {
+    bash "${XUI_ASSISTANT_DIR}/core/database_transfer.sh"
 }
 
-manage_users() {
-    python3 /root/xui-assistant/core/user_managment.py
+xui_assis_gift_users() {
+    python3 "${XUI_ASSISTANT_DIR}/core/day_size.py"
 }
 
-check_xui_exist() {
-    local file_path="/etc/x-ui/x-ui.db"
-    local status
-    
-    if [ -f "$file_path" ]; then
-        status="${GREEN}Installed"${NC}
-    else
-        status=${RED}"Not installed"${NC}
-    fi
-    
-    echo "$status"
+xui_assis_manage_admins() {
+    python3 "${XUI_ASSISTANT_DIR}/core/user_managment.py"
 }
 
-transfer_db() {
-    bash  /root/xui-assistant/core/database_transfer.sh
+xui_assis_set_xray_restart() {
+    python3 "${XUI_ASSISTANT_DIR}/core/setup_cron.py"
 }
 
-fix_timezone() {
-    sudo timedatectl set-timezone UTC
-    echo -e "${GREEN}Timezone set to UTC.${NC}"
+xui_assis_fix_whatsapp_time() {
+    timedatectl set-timezone UTC
+    echo -e "${GREEN}Timezone set to UTC for WhatsApp compatibility.${NC}"
 }
 
-install_wordpress() {
+xui_assis_install_wordpress() {
     bash <(curl -Ls https://raw.githubusercontent.com/dev-ir/WordPress-Installer/master/main.sh)
 }
 
-block_speedtest_sites() {
+xui_assis_block_speedtest() {
     bash <(curl -Ls https://raw.githubusercontent.com/dev-ir/speedtest-ban/master/main.sh)
 }
 
-xray_restart() {
-    python3 /root/xui-assistant/core/setup_cron.py
+xui_assis_user_management_bot() {
+    python3 "${XUI_ASSISTANT_DIR}/core/v2ray_bot.py"
 }
 
-xui_bot() {
-    python3 /root/xui-assistant/core/v2ray_bot.py
+xui_assis_install_multi_panels() {
+    bash "${XUI_ASSISTANT_DIR}/core/mlxui.sh"
 }
 
-xui_managment(){
-    bash /root/xui-assistant/core/mlxui.sh
+xui_assis_add_subscription_templates() {
+    bash <(curl -Ls https://raw.githubusercontent.com/dev-ir/xui-subscription-template/refs/heads/master/main.sh )
 }
 
-uninstall() {
+xui_assis_uninstall() {
     echo -e "${GREEN}Uninstalling XUI-ASSISTANT...${NC}"
-    rm -rf /root/xui-assistant/
-    rm -rf /usr/local/bin/xui-assis
-    echo -e "${RED}XUI-ASSISTANT Uninstalled.${NC}"
+    rm -rf "$XUI_ASSISTANT_DIR"
+    rm -f /usr/local/bin/xui-assis
+    echo -e "${RED}XUI-ASSISTANT has been completely removed.${NC}"
 }
 
-exit_program() {
-    echo -e "${GREEN}Exiting program...${NC}"
+xui_assis_exit() {
+    echo -e "${GREEN}Exiting the program...${NC}"
     exit 0
 }
 
-loader
+xui_assis_main_menu() {
+    local menu_options
+    read -r -d '' menu_options << EOM
+| 1  - Transfer Database to Another Server
+| 2  - Gift Flow/Time to All Users
++-----------------------------------------------------------------------------------------+
+| 3  - Admin Management
+| 4  - User Management Bot (Traffic/Date)
++-----------------------------------------------------------------------------------------+
+| 5  - Schedule Xray Service Restart
+| 6  - Fix WhatsApp Date/Time Issue
+| 7  - Block Speedtest Websites
++-----------------------------------------------------------------------------------------+
+| 8  - Install WordPress Alongside X-UI
+| 9  - Install More X-UI Panels
+| 10 - Add Subscription Templates
++-----------------------------------------------------------------------------------------+
+| 11 - Uninstall This Script
+| 0  - Exit Program
+EOM
+    
+    xui_assis_display_menu "$menu_options"
+    echo -ne "${YELLOW}| Enter option number: ${NC}"
+    read -r choice
+    case $choice in
+        1) xui_assis_transfer_database ;;
+        2) xui_assis_gift_users ;;
+        3) xui_assis_manage_admins ;;
+        4) xui_assis_user_management_bot;;
+        5) xui_assis_set_xray_restart ;;
+        6) xui_assis_fix_whatsapp_time ;;
+        7) xui_assis_block_speedtest ;;
+        8) xui_assis_install_wordpress;;
+        9) xui_assis_install_panels ;;
+        10) xui_assis_add_subscription_templates ;;
+        11) xui_assis_uninstall ;;
+        0) xui_assis_exit ;;
+        *) echo -e "${RED}Invalid option. Please try again.${NC}" ;;
+    esac
+}
+
+# Start the program
+xui_assis_main_menu
